e19bbf8e5469fc89357a47178cfc71b15a28c5a9
rabindhakal404 <rabindhakal00000@gmail.com>
2026-04-29 23:30:19 +0545

    fix neon ssl connection

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -5,7 +5,9 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
